5acdcbb5e3839967f33db5762314baf49443c845
oceans <oceans-mail@qq.com>
2020-08-30 15:34:55 +0800

    add

--- src/init/init-app.js
+++ src/init/init-app.js
@@ -10,18 +10,7 @@ window.ele = new Vue({
   el: '#ele',
 })
 
-
 initOrToggleSidebar(true)
 initOrToggleTheme(true)
 initOrToggleCodeColor(true)
 
-
-/*+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
-                                 防止普通人偷窥，无脑复制粘贴，造成SEO污染
-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-*/
-// $(document).bind('contextmenu', function() {
-//   return false
-// })
-// $(document).bind('selectstart', function() {
-//   return false
-// })
