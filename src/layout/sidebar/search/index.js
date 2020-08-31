d23eb1063a191e85530c83c38de5ae26a3043768
oceans <oceans-mail@qq.com>
2020-08-31 16:53:00 +0800

    修复侧边栏的找一找ajax延后的问题

--- src/layout/sidebar/search/index.js
+++ src/layout/sidebar/search/index.js
@@ -1,6 +1,17 @@
 require('./_search.scss')
-$('#btnZzk').remove()
-const $q = $('#q')
-$q.parent()
-  .html($q)
-  .append('<span class="search-icon" onclick="zzk_go()"><i class="el-icon-search"></i></span>')
+
+function beautifyZzk() {
+  $('#btnZzk').remove()
+  const $q = $('#q')
+  $q.parent()
+    .html($q)
+    .append('<span class="search-icon" onclick="zzk_go()"><i class="el-icon-search"></i></span>')
+}
+
+beautifyZzk()
+
+$(document).on('ajaxLater', function(e, item) {
+  if (item.url === `/${g.userPath}/ajax/sidecolumn.aspx`) {
+    beautifyZzk()
+  }
+})
